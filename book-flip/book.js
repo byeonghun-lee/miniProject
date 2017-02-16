e2870d00aa597d612c60f037b4f45512dd5eb96e
DESKTOP-ABVBI8V\hun08 <byeonghun08@gmail.com>
2017-02-16 13:36:37 +0900

    돌아가는 책

--- book-flip/book.js
+++ book-flip/book.js
@@ -4,11 +4,13 @@ var first_book_flip=book_flip[0];
 // console.log(book_flip);
 for (var i=0; i < book_flip.length; i++) {
   var book = book_flip[i];
+  // console.log(book);
   var flip_name = book.children[0];
   var flip_cover  = book.children[book.children.length - 1];
   flip_name.onclick = function() {
-    this.parentNode.style.cssText = 'transform: rotateY(90deg) translateX(100%)';
+    this.parentNode.style.cssText = 'transform: rotateY(80deg) translateX(100%)';
   };
+  console.log(flip_cover);
   flip_cover.onclick = function() {
     this.parentNode.style.cssText = '';
   };
